549da28834413eb8d8a5a9dba0893f3341eca2c6
Documenter.jl <documenter@juliadocs.github.io>
2021-11-22 22:44:16 +0000

    build based on d39cb1d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnrollingAverages","category":"page"},{"location":"#UnrollingAverages","page":"Home","title":"UnrollingAverages","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnrollingAverages.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnrollingAverages]","category":"page"}]
+}
