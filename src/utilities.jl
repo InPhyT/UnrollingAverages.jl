c6a19447178cc52fdfa331c784e9986e2637473d
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2021-11-24 19:16:57 +0100

    add tests
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>

--- src/utilities.jl
+++ src/utilities.jl
@@ -7,4 +7,4 @@ function pad_left_right(v::Vector{Float64},l::Int64, r::Int64; p::T = 0) where {
 end
 
 moving_average(time_series::Union{Vector{Float64},Vector{Int64}}, n₋::Int64, n₊::Int64) = [mean(time_series[(i-n₋):(i+n₊)]) for i in (1+n₋):(length(time_series)-n₊)]
-moving_average(time_series::Union{Vector{Float64},Vector{Int64}},window::Int64) = moving_average(time_series,0,window)
+moving_average(time_series::Union{Vector{Float64},Vector{Int64}},window::Int64) = moving_average(time_series,0,window-1)
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,20 +11,24 @@ using Test
 
     # test series
     for i in 1:10
-        original_time_series = [rand(0:9) for i in 1:500];
+        original_time_series = [rand(0:9) for i in 1:500]
 
         averaged_time_series = [mean(original_time_series[(i-n₋):(i+n₊)]) for i in (1+n₋):(length(original_time_series)-n₊)];
         
         # test unroll_recursive
         @test unroll(averaged_time_series, n₋ + n₊ + 1; initial_conditions =  Tuple(original_time_series[1:(n₋+n₊)])  )[1] == original_time_series
+        # test unroll_recursive error when initial values are not of the correct length
+        @test unroll(averaged_time_series, n₋ + n₊ + 1; initial_conditions =  Tuple(original_time_series[1:(n₋+n₊)])  )[1] == original_time_series
         ## test that unroll_recursive errors when initial conditions are of wrong length
-        @test_throws ErrorException unroll(averaged_time_series, n₋ + n₊ + 1; initial_conditions =  Tuple(original_time_series[1:(n₋+n₊+5)])  )[1]
+        @test_throws ErrorException unroll(averaged_time_series, n₋ + n₊ + 1; initial_conditions =  Tuple(original_time_series[2:(n₋+n₊)])  )[1]
         # test that assert_positive_integer has no effects when `initial_conditions` are specified (calls unroll_recursive again)
         @test unroll(averaged_time_series, n₋ + n₊ + 1; initial_conditions =  Tuple(original_time_series[1:(n₋+n₊)]), assert_positive_integer = true  )[1] == original_time_series
         # test unroll_linear_approximation
         @test UnrollingAverages.moving_average(unroll(averaged_time_series, n₋ + n₊ + 1 )[1] , n₋, n₊ ) ≈ averaged_time_series
         # test unroll_iterative
         @test original_time_series ∈ unroll(averaged_time_series, n₋ + n₊ + 1;  assert_positive_integer = true)
+        # test internal `moving_average(time_series::Union{Vector{Float64},Vector{Int64}},window::Int64)`
+        @test UnrollingAverages.moving_average(unroll(averaged_time_series, n₋ + n₊ + 1 )[1] , n₋ + n₊ +1 ) ≈ averaged_time_series
 
         #@time unroll(averaged_time_series, n₋ + n₊ + 1;  assert_positive_integer = true)
 
